8d4044ce578ac89faa916c9088e6748f50c2f607
ramen0702 <zettai72masa@gmail.com>
2022-05-18 01:06:24 +0900

    [update] Chenge DarkButton design

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
+// Initial operation
 window.onload = function(){
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         UserFlg = true;
         SetDerk = true;
+        let checkbox2 = document.getElementById('btn-mode');
+        checkbox2.checked = true;
     }else{
         UserFlg = false;
         SetDerk = false;
     }
 }
+
+// Start interval operation
+setInterval(Check, 1000);
+
+// Interval operation
 function Check(){
     if (!(window.matchMedia('(prefers-color-scheme: dark)')).matches && UserFlg) {
-        // ライトモード
-        document.body.classList.remove("dark-theme");
-        document.body.classList.add("light-theme");
+        // Change LightMode
+        ModeChange("light");
         UserFlg = false;
         SetDerk = false;
     }else if(window.matchMedia('(prefers-color-scheme: dark)').matches && !(UserFlg)){
-        // ダークモード
-        document.body.classList.remove("light-theme");
-        document.body.classList.add("dark-theme");
+        // Chenge DarkMode
+        ModeChange("dark");
         UserFlg = true;
         SetDerk = ture;
     }
 }
 
-setInterval(Check, 1000);
-
-// チェックボックスの取得
+// Get CheckBox state
 const btn = document.querySelector("#btn-mode");
 
-// チェックした時の挙動
+// Operation when checked
 btn.addEventListener("change", () => {
     if (SetDerk) {
-        // ライトモード
-        document.body.classList.remove("dark-theme");
-        document.body.classList.add("light-theme");
+        // Change LightMode
+        ModeChange("light");
         SetDerk = false;
     } else {
-        // ダークモード
-        document.body.classList.remove("light-theme");
-        document.body.classList.add("dark-theme");
+        // Chenge DarkMode
+        ModeChange("dark");
         SetDerk = true;
     }
 });
+
+function ModeChange(mode){
+    if(mode == "dark"){
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
+    }else{
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    }
+}
